9b6711f4a0f2f3561764cd7a348e8fdd4c3b016e
MIrzoyev1 <ali.mirzoyev.amr2@gmail.com>
2024-11-18 11:01:12 +0000

    s

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1 = "Holberton"
-str2 = "School"
-print(str1 + str2 + str1)
-print(str1)
+str1 = "Welcome to"
+str2 = "Holberton School!"
+print(str1 + " " + str2)
+"Welcome to Holberton School!"
