ff23fd3c63c5636871f2ecf221e7c84744527ca0
MIrzoyev1 <ali.mirzoyev.amr2@gmail.com>
2024-11-19 14:22:39 +0000

    fix

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
-    """Check if a character is lowercase.""" 
+    letter = ord(c) 
     if ord(c) >= 97 and ord(c) <= 122:
         return True
-    else:
+    elif letter in range(65, 90):
         return False
