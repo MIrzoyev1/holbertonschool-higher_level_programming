620821fc215ff1a1dd036ffd7497482ab487c211
MIrzoyev1 <ali.mirzoyev.amr2@gmail.com>
2024-11-19 14:05:31 +0000

    fix

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -4,5 +4,5 @@ for i in range(0,10):
     for j in range(0, 10):  
         if i<j and (i!=8 or j!=9):  
             print("{}{}, ".format(i, j), end="")
-        else:
-            print("{}{}".format(i, j))
+        if i==8 and j==9:
+            print("{}{}".format(i, j),end="\n")
