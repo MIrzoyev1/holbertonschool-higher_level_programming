95930f5f1726997150b08c1afab228554a5030a1
MIrzoyev1 <ali.mirzoyev.amr2@gmail.com>
2024-11-19 13:48:11 +0000

    fix

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
-for i in range(97, 123):  
-    if i != 113 and i != 101:  
-        print("{}".format(chr(i)), end="")                                         
+for i in range(97, 123):  # ASCII range for lowercase letters
+    if i != 113 and i != 101:  # Exclude 'q' (113) and 'e' (101)
+        print("{}".format(chr(i)), end="")   
