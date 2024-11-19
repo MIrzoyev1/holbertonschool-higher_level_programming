c117d4bfc98be28c7005ec48febdf7c021a79beb
MIrzoyev1 <ali.mirzoyev.amr2@gmail.com>
2024-11-19 11:51:37 +0000

    s

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for i in range (97, 123):
-print("{}".format(chr(i)), end="")
+for i in range(97, 123):  # ASCII values for lowercase 'a' to 'z'
+    print("{}".format(chr(i)), end="")
+
