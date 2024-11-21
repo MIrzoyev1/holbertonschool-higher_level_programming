16c5c5d55283e71b3c418630483fafeaab741e63
MIrzoyev1 <ali.mirzoyev.amr2@gmail.com>
2024-11-21 09:11:09 +0000

    s

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
-s1 = Square()
 print(s1.area())
 
-s2 = Square(4)
 print(s2.area())
 
-s2.size = 5
 print(s2.area())
 
 try:
-    s3 = Square(-3)
-except ValueError as e:
+
     print(e)
 
 try:
-    s4 = Square("size")
-except TypeError as e:
     print(e)
