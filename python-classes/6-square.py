c32c0866cf2e4c8bf8e8422dba13d52947a30b19
MIrzoyev1 <ali.mirzoyev.amr2@gmail.com>
2024-11-21 12:07:48 +0000

    fix

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""Module defining a Square class with size, position, area, and printing functionality"""
+"""Module defining a Square class with size, position, area, and printing."""
+
 
 class Square:
-    """Square class with size and position"""
-    
+    """Square class with size and position."""
+
     def __init__(self, size=0, position=(0, 0)):
         """Initialize a Square instance.
 
         Args:
             size (int, optional): The size of the square. Defaults to 0.
-            position (tuple, optional): The position of the square. Defaults to (0, 0).
+            position (tuple, optional): The position of the square.
+                Defaults to (0, 0).
         """
         self.size = size
         self.position = position
@@ -52,8 +54,8 @@ class Square:
             TypeError: If position is not a tuple of 2 positive integers.
         """
         if (not isinstance(value, tuple) or
-            len(value) != 2 or
-            not all(isinstance(x, int) and x >= 0 for x in value)):
+                len(value) != 2 or
+                not all(isinstance(x, int) and x >= 0 for x in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
@@ -79,7 +81,8 @@ class Square:
         for _ in range(self.__position[1]):
             print()
 
-        # Print the square with the horizontal space (position[0]) and '#' characters
+        # Print the square with the horizontal space (position[0]) 
+        # and '#' characters
         for _ in range(self.__size):
             print(" " * self.__position[0] + "#" * self.__size)
 
