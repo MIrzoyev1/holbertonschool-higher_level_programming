48df10db21a6668029bc7efe2c9d53a5484e0945
MIrzoyev1 <ali.mirzoyev.amr2@gmail.com>
2024-11-21 09:14:26 +0000

    s

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,13 +1,26 @@
 #!/usr/bin/python3
-print(s1.area())
 
-print(s2.area())
+# Assuming s1 and s2 are already defined as Square instances
+s1 = Square()  # size = 0
+s2 = Square(4)  # size = 4
 
-print(s2.area())
+# Print the area for s1 and s2
+print(s1.area())  # Output: 0
+print(s2.area())  # Output: 16
 
+# Change size of s2
+s2.size = 5
+print(s2.area())  # Output: 25
+
+# Try creating a square with invalid size (-3), which should raise an exception
 try:
+    s3 = Square(-3)  # This will raise a ValueError
+except ValueError as e:
+    print(e)  # This will print "size must be >= 0"
 
-    print(e)
-
+# Try creating a square with invalid size
+("size"), which should raise a TypeError
 try:
-    print(e)
+    s4 = Square("size")  # This will raise a TypeError
+except TypeError as e:
+    print(e)  # This will print "size must be an integer"
