45c96fd8de87101130e59b5b6012cbad748780ad
MIrzoyev1 <ali.mirzoyev.amr2@gmail.com>
2024-11-22 10:50:42 +0000

    fix

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -16,7 +16,7 @@ class Rectangle:
     number_of_instances = 0
     print_symbol = "#"
 
-    def __init__(self, width=0, height=0):
+    def __init__(self, width, height):
         """Initialize a new Rectangle instance"""
         self.width = width
         self.height = height
